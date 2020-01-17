4adb4c0d05ccbe01da10f487d42effb615b0ae9f
Sebastian Kraft <sebastian.kraft@affilicon.org>
2020-01-17 10:33:58 +0100

    feat: add methods to use math on vectors with vectors

--- src/vector2.ts
+++ src/vector2.ts
@@ -5,4 +5,39 @@ export default class Vector2 {
     this.x = x;
     this.y = y;
   }
+
+  add(addend: Vector2): Vector2 {
+    return new Vector2(
+      this.x + addend.x,
+      this.y + addend.y
+    );
+  }
+
+  subtract(addend: Vector2): Vector2 {
+    return new Vector2(
+      this.x - addend.x,
+      this.y - addend.y
+    );
+  }
+
+  multiplyByNumber(multiplicand: number): Vector2 {
+    return new Vector2(
+      this.x * multiplicand,
+      this.y * multiplicand
+    );
+  }
+
+  multiply(multiplicand: number): Vector2 {
+    return new Vector2(
+      this.x * multiplicand,
+      this.y * multiplicand
+    );
+  }
+
+  divide(division: number): Vector2 {
+    return new Vector2(
+      this.x / division,
+      this.y / division
+    );
+  }
 }
